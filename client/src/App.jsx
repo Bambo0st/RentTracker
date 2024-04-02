2138a8f668d32f08cc9d4198a581da31ef16303b
Adithya Nagaraja <adibamboost@gmail.com>
2024-04-02 10:40:01 +0530

    Start commit

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl underline font-bold text-red-500">Hello</h1>
+  )
+}
