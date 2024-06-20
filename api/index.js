f76629ad0da7ea33cd9c79326e0b2f25d40fa70e
Adithya Nagaraja <adibamboost@gmail.com>
2024-06-20 18:03:21 +0530

    Production2

--- api/index.js
+++ api/index.js
@@ -28,8 +28,9 @@ app.use("/api/property", propertyRouter);
 app.use("/api/payment", paymentRouter);
 
 app.use(express.static(path.join(__dirname, '/client/dist')))
-app.get('*', path.join(__dirname, 'client', 'dist', 'index.html'))
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
