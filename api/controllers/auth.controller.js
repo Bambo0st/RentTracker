6ecfa380915daca7415b8d26170eee4dd83e115e
Adithya Nagaraja <adibamboost@gmail.com>
2024-10-27 12:16:09 +0530

    Fixed errors in sign-in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,9 +28,9 @@ export const signin = async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if (!validUser) return next(new errorHandler(404, "User Not Found!"))
+        if (!validUser) return next(errorHandler(404, "User Not Found!"))
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(new errorHandler(401, "Wrong Credentials Entered!"))
+        if (!validPassword) return next(errorHandler(401, "Wrong Credentials Entered!"))
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const { password: pass, ...rest } = validUser._doc;
         res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
